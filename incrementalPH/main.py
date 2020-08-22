aa89a92d1be8ff849f8f9c6d9e473696464d3f38
Anindya Moitra <id.anindya@gmail.com>
2020-08-21 22:15:41 -0500

    Began implementing the fully incremental PH.

---
+++ incrementalPH/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.spatial import distance_matrix
+import statistics
+import random
+
+
+data = np.genfromtxt('testData.csv', delimiter=',', skip_header=1)   # Load the entire data as a numpy array.
+dim = data.shape[1]   # Get the dimension of the data.
+
+window = np.empty([0, dim])   # Create an empty sliding window.
+windowMaxSize = 200   # Set the maximum number of data points the window may contain.
+pointCounter = 0
+
+windowKeys = []
+partitionLabels = []
+avgNNDistPartitions = {}
