5951ceedbaf1bcce76301ad97a4a07ec0a124269
Anindya Moitra <id.anindya@gmail.com>
2020-11-20 22:30:05 -0500

    Compare the two simplices when one has filter value greater than that of the other.

--- incrementalPH/persistenceIntervals.py
+++ incrementalPH/persistenceIntervals.py
@@ -3,8 +3,15 @@
 
 # Function to compare two simplices that helps create the total ordering of the simplices.
 # Each simplex is represented as a list, bundled with its filter value: [simplex, filter value] e.g. [{0,1}, 4]
-def compare(simplex1, simplex2):
+def compareSimplices(simplex1, simplex2):
     if len(simplex1[0]) == len(simplex2[0]):  # If both simplices have the same dimension
         if simplex1[1] == simplex2[1]:  # If both simplices have the same filter value
             if sum(simplex1[0]) > sum(simplex2[0]):  # Break the tie
                 return 1
+            else:
+                return -1
+        else:  # If the two simplices do not have the same filter value
+            if simplex1[1] > simplex2[1]:  # If the filter value of simplex1 is greater than that of simplex2
+                return 1
+            else:
+                return -1
