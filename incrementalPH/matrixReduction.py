348fbadb4a8458825f7d68b41489c4bfdc7a0264
Anindya Moitra <id.anindya@gmail.com>
2020-12-06 16:41:12 -0500

    Utility functions for the reduction of the boundary matrix.

---
+++ incrementalPH/matrixReduction.py
@@ -0,0 +1,25 @@
+# Implementation of the standard algorithm for the reduction of the boundary matrix.
+# Courtesy: A. Zomorodian and G. Carlsson, “Computing persistent homology,” 2005, and outlace.com
+
+# A function to return the row index of the lowest 1 (pivot) in a column i in the boundary matrix
+def low(i, matrix):
+    col = matrix[:, i]
+    colLen = len(col)
+    for i in range((colLen-1), -1, -1):  # Scan the column from the bottom until we find the first 1
+        if col[i] == 1:
+            return i
+
+    # If the pivot of column i does not exist (e.g. in a column of all zeros), return -1 (pivot 'undefined')
+    return -1
+
+
+# A function to check if the boundary matrix is fully reduced
+def isReduced(matrix):
+    for j in range(matrix.shape[1]):  # Iterate through columns
+        for i in range(j):  # Scan the columns until we reach column j
+            low_j = low(j, matrix)
+            low_i = low(i, matrix)
+            if low_j == low_i and low_j != -1:
+                return i, j  # Return column i that will be added to column j
+
+    return [0, 0]
